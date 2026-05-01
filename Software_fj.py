1ec8a6d189a35294cd0ff0e1114708e9fa556c74
Oscar_Ramirez <oarc1986@gmail.com>
2026-04-30 23:33:13 -0500

    Version1

--- Software_fj.py
+++ Software_fj.py
@@ -1,4 +1,9 @@
 #Tarea 4 POO
+import tkinter as tk
+from tkinter import messagebox
+from abc import ABC,abstractmethod
+from datetime import datetime
+
 class Cliente:
     def __init__(self,nombre,id,edad,mail):
         self._nombre=nombre
