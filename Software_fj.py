8ec1ebdfc06eeab197e4cf8d6a5d62d1154e041f
Oscar_Ramirez <oarc1986@gmail.com>
2026-05-04 21:46:35 -0500

    Versión1

--- Software_fj.py
+++ Software_fj.py
@@ -17,6 +17,19 @@ class ValidacionError(SistemaError):
 class ReservaError(SistemaError):
     pass
 
+#Clase abstracta
+class Servicio(ABC):
+    def __init__(self, nombre):
+        self._nombre = nombre
+
+    @abstractmethod
+    def calcular_costo(self, duracion):
+        pass
+
+    @abstractmethod
+    def descripcion(self):
+        pass
+
 # Clientes
 class Cliente:
     def __init__(self, nombre, id, edad, mail):
@@ -57,4 +70,81 @@ class Cliente:
 
     def mostrar_info(self):
         return f"{self.nombre} ({self.edad}) - {self.mail}"
+
+#Servicios
+class ReservaSala(Servicio):
+    def calcular_costo(self, duracion):
+        return duracion * 50
+
+    def descripcion(self):
+        return "Reserva de Sala"
+
+
+class AlquilerEquipo(Servicio):
+    def calcular_costo(self, duracion):
+        return duracion * 30
+
+    def descripcion(self):
+        return "Alquiler de Equipo"
+
+
+class Asesoria(Servicio):
+    def calcular_costo(self, duracion):
+        return duracion * 100
+
+    def descripcion(self):
+        return "Asesoría Especializada"
+
+#Reserva
+class Reserva:
+    def __init__(self, cliente, servicio, duracion):
+        self.cliente = cliente
+        self.servicio = servicio
+        self.duracion = duracion
+        self.estado = "pendiente"
+
+    def confirmar(self):
+        if self.estado != "pendiente":
+            raise ReservaError("Ya confirmada o cancelada")
+        self.estado = "confirmada"
+
+    def calcular_total(self):
+        try:
+            return self.servicio.calcular_costo(self.duracion)
+        except Exception as e:
+            raise ReservaError("Error al calcular costo") from e
+
+#Sistema
+class Sistema:
+    def __init__(self):
+        self.reservas = []
+
+    def crear_reserva(self, nombre, email, tipo_servicio, duracion):
+        try:
+            cliente = Cliente(nombre, email)
+
+            if tipo_servicio == "Sala":
+                servicio = ReservaSala()
+            elif tipo_servicio == "Equipo":
+                servicio = AlquilerEquipo()
+            else:
+                servicio = Asesoria()
+
+            reserva = Reserva(cliente, servicio, int(duracion))
+            total = reserva.calcular_total()
+
+            reserva.confirmar()
+            self.reservas.append(reserva)
+
+            return total
+
+        except Exception as e:
+            registrar_log(str(e))
+            raise
+
+
+
+
+
+
 
