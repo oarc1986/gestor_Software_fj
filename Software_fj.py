26ce4c45e236d8cf2b41e20d32575d6a85717348
Oscar_Ramirez <oarc1986@gmail.com>
2026-05-04 21:27:32 -0500

    Versión2_MIguel_Gordillo

--- Software_fj.py
+++ Software_fj.py
@@ -17,5 +17,44 @@ class ValidacionError(SistemaError):
 class ReservaError(SistemaError):
     pass
 
+# Clientes
+class Cliente:
+    def __init__(self, nombre, id, edad, mail):
+        self.nombre = nombre
+        self.id = id
+        self.edad = edad
+        self.mail = mail
 
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, valor):
+        if not valor or len(valor) < 3:
+            raise ValidacionError("Nombre inválido")
+        self._nombre = valor
+
+    @property
+    def edad(self):
+        return self._edad
+
+    @edad.setter
+    def edad(self, valor):
+        if valor <= 0:
+            raise ValidacionError("Edad inválida")
+        self._edad = valor
+
+    @property
+    def mail(self):
+        return self._mail
+
+    @mail.setter
+    def mail(self, valor):
+        if "@" not in valor:
+            raise ValidacionError("Email inválido")
+        self._mail = valor
+
+    def mostrar_info(self):
+        return f"{self.nombre} ({self.edad}) - {self.mail}"
 
